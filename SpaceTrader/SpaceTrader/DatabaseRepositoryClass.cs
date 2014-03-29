888302699b5534ebfa5f93bf3ebd3c500dae7e6f
Christian T <ctamakloe@gmail.com>
2014-03-29 19:53:19 +0000

    Add mock database class

---
+++ SpaceTrader/SpaceTrader/DatabaseRepositoryClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTrader
+{
+    public class DatabaseRepositoryClass
+    {
+        IDatabaseAccess da;
+
+        public DatabaseRepositoryClass(IDatabaseAccess da_in)
+        {
+            da = da_in;
+        }
+
+        public int getPrice(string iteName)
+        {
+
+            string query = "SELECT price FROM setting WHERE name='" + iteName + "'";
+            int price = da.Query_GetPriceForName(query);
+            return price;
+
+        }
+    }
+
+    public interface IDatabaseAccess
+    {
+        int Query_GetPriceForName(string query);
+    }
+
+}
+
+
